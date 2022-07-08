16f708c9345c6f70273bdc13fe40bcba57ce866c
weiwei <15952037019@163.com>
2022-07-08 13:50:03 +0800

    update UserMapper

--- edu_home_parent/ssm_dao/src/main/java/com/wei/dao/UserMapper.java
+++ edu_home_parent/ssm_dao/src/main/java/com/wei/dao/UserMapper.java
@@ -54,4 +54,12 @@ public interface UserMapper {
     public List<Resource> findResourceByRoleId(List<Integer> ids);
     
     public List<Resource> findResourceByRoleId2(List<Integer> ids);
+
+    public void test1();
+    public void test2();
+    public void test3();
+    public void test4();
+    public void test5();
+    public void test6();
+    public void test7();
 }
